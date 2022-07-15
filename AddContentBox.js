2b70f6c8f6f9520a07d5aa758b749f55e005d9c7
Mayur Maheshwari <u0649996@utah.edu>
2022-07-15 15:45:35 +0530

    Update AddContentBox.js

--- AddContentBox.js
+++ AddContentBox.js
@@ -1,2 +1,2 @@
 Shopify.Checkout.OrderStatus.addContentBox(`<h1>You may be interested in :</h1>
-<img src="https://cdn.shopify.com/s/files/1/0062/9587/9780/products/worlds-favourite-flavoured-instant-coffee-515828_1024x1024.jpg?v=1648563280" alt="Trulli" width="500" height="333">`)
+<img src="https://cdn.shopify.com/s/files/1/0119/2742/0992/products/2021-11-15-1-46-26_product_2_1636920986302_2000x.jpg?v=1636920998" alt="Trulli" width="500" height="333">`)
